48d08fb8020ae6a116bff86c23dc1b49ce7b51ab
Jain <saurabh.y.jain@accenture.com>
2024-10-17 14:50:18 -0400

    change app comments again

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to Saurabh's repository
+          Edit <code>src/App.jsx</code> and save to Saurabh second test without local build
         </p>
       </div>
       <p className="read-the-docs">
